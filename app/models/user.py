0bc20fbf29e3b1839ced0c585e5cb1131018133f
Sean Pan <seanhpan@gmail.com>
2021-12-14 13:41:18 -0800

    seeder and database working

--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,5 @@
 from .db import db
+from sqlalchemy.sql import func
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
@@ -13,7 +14,7 @@ class User(db.Model, UserMixin):
     profile_pic = db.Column(db.String(255), nullable=False, default='https://media.discordapp.net/attachments/917541871457275925/918846475897798727/default-user.jpeg')
     cover_photo = db.Column(db.String(255), nullable=False, default='https://res.cloudinary.com/photofinder/image/upload/v1639506962/orionthemes-placeholder-image_twhbxf.jpg')
     hashed_password = db.Column(db.String(255), nullable=False)
-    createdAt = db.Column(db.DateTime, nullable=False)
+    createdAt = db.Column(db.DateTime(timezone=False), default=func.now())
 
     @property
     def password(self):
@@ -29,8 +30,12 @@ class User(db.Model, UserMixin):
     def to_dict(self):
         return {
             'id': self.id,
-            'username': self.username,
-            'email': self.email
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'email': self.email,
+            'profile_pic': self.profile_pic,
+            'cover_photo': self.cover_photo,
+            'createdAt': self.createdAt
         }
 
     posts = db.relationship('Post', back_populates='users', cascade='all, delete-orphan')
--- app/seeds/users.py
+++ app/seeds/users.py
@@ -3,16 +3,17 @@ from app.models import db, User
 
 # Adds a demo user, you can add other users here if you want
 def seed_users():
-    demo = User(
-        username='Demo', email='demo@aa.io', password='password')
-    marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
-    bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password')
+    demo = User(first_name='Demo', last_name='User', email='demo@aa.io', password='password')
+    Sean = User(first_name='Sean', last_name='Pan', email='span@gmail.com', password='password')
+    Sharon = User(first_name='Sharon', last_name='Truong', email='struong@aa.io', password='password')
+    # marnie = User(
+    #     username='marnie', email='marnie@aa.io', password='password')
+    # bobbie = User(
+    #     username='bobbie', email='bobbie@aa.io', password='password')
 
     db.session.add(demo)
-    db.session.add(marnie)
-    db.session.add(bobbie)
+    db.session.add(Sean)
+    db.session.add(Sharon)
 
     db.session.commit()
 
