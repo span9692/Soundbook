aa954d39110a6a5639fca5eb7ca1261ad6a4edbb
Sean Pan <seanhpan@gmail.com>
2021-12-14 10:37:13 -0800

    post models done

--- app/models/post.py
+++ app/models/post.py
@@ -4,3 +4,9 @@ class Post(db.Model):
     __tablename__ = 'posts'
 
     id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    profile_pic = db.Column(db.String(255), nullable=False, default='https://media.discordapp.net/attachments/917541871457275925/918846475897798727/default-user.jpeg')
+    cover_photo = db.Column(db.String(255), nullable=False, default='https://res.cloudinary.com/photofinder/image/upload/v1639506962/orionthemes-placeholder-image_twhbxf.jpg')
+    createdAt = db.Column(db.DateTime, nullable=False)
