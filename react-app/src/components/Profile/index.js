b05d98c48444eefe110a4f22f27dfa0b880609be
Sean Pan <seanhpan@gmail.com>
2021-12-15 15:39:25 -0800

    profile top css done

--- react-app/src/components/Profile/index.js
+++ react-app/src/components/Profile/index.js
@@ -7,33 +7,26 @@ function Profile() {
 
     return (
         <>
-        <div className='profile-container'>
-            <div className='profile-background-color'>
-                <div className='profile-images'>
-                    <img className='cover-photo' src={user.cover_photo}></img>
-                    <img className='profile-photo' src='https://res.cloudinary.com/photofinder/image/upload/v1639338101/kid%20friendly%20profile%20pics/HtwPZgej_400x400_itffcg.jpg'></img>
-                    <div className='profile-content'>
-                        {user.first_name} {user.last_name}
+            <div>
+                <div className='profile-background-color'>
+                    <div className='profile-images'>
+                        <img className='cover-photo' src={user.cover_photo}></img>
+                        <img className='profile-photo' src='https://res.cloudinary.com/photofinder/image/upload/v1639338101/kid%20friendly%20profile%20pics/HtwPZgej_400x400_itffcg.jpg'></img>
+                        <div className='profile-content'>
+                            {user.first_name} {user.last_name}
+                        </div>
                     </div>
-                </div>
-                <hr style={{marginTop:  1+'rem', marginBottom: 1+'rem'}} size='1' width='60%' color='#dddfe2'></hr>
-                <div className='profile-nav'>
-                    <div className='nav-links'>
-                        <div className='profile-nav-links'>Posts</div><div className='profile-nav-links'>About</div><div className='profile-nav-links'>Friends</div><div className='profile-nav-links'>Photos</div>
-                    </div>
-                    <div className='edit-profile-btn'>
-                        <div>Edit Profile</div>
+                    <hr style={{marginTop:  1+'rem', marginBottom: 1+'rem'}} size='1' width='60%' color='#dddfe2'></hr>
+                    <div className='profile-nav'>
+                        <div className='nav-links'>
+                            <div className='profile-nav-links'>Posts</div><div className='profile-nav-links'>About</div><div className='profile-nav-links'>Friends</div><div className='profile-nav-links'>Photos</div>
+                        </div>
+                        <div className='edit-profile-btn'>
+                            <div>Edit Profile</div>
+                        </div>
                     </div>
                 </div>
             </div>
-            {/* <div className='profile-nav profile-background-color'>
-                <div className='sideColumn'></div>
-                <div className='midColumn'>
-                    <span>Posts</span><span>About</span><span>Friends</span><span>Photos</span>
-                </div>
-                <div className='sideColumn'></div>
-            </div> */}
-        </div>
         </>
     )
 }
