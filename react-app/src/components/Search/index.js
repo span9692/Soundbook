4d0d7d0928b75ceae384d3fac3cd405698d75188
Sean Pan <seanhpan@gmail.com>
2021-12-22 10:05:17 -0800

    search bar done

--- react-app/src/components/Search/index.js
+++ react-app/src/components/Search/index.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react"
+import { Redirect, useHistory } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { searchUser } from "../../store/search";
 import './search.css'
 
 function Search() {
     const [searchParams, setSearchParams] = useState('');
+    const history = useHistory();
     const dispatch = useDispatch()
 
     const searchResults = useSelector(state => Object.values(state.search))
@@ -14,7 +16,12 @@ function Search() {
         dispatch(searchUser(searchParams))
     }, [dispatch, searchParams])
 
-
+    const foundUser = (userId) => {
+        console.log(userId)
+        setSearchParams('')
+        history.push(`/users/${userId}`)
+        // <Redirect to={`/users/${userId}`}></Redirect>
+    }
 
     return (
         <div className='search-container'>
@@ -32,7 +39,7 @@ function Search() {
             <div className='search-result-container'>
                 <span className='search-header'><span className='result-text'>Results</span> for '{searchParams}'</span>
                 {searchResults.map(result => (
-                <div key={result.id} className='search-result-row'>
+                <div key={result.id} className='search-result-row pointer' onClick={() => foundUser(result.id)}>
                     <img className='search-portrait' src={result.profile_pic}></img>
                     <div>{result.first_name} {result.last_name}</div>
                 </div>
