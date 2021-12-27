91f99ac6a048fb9ac25d8bbd1eb0672ea0d9018a
Sean Pan <seanhpan@gmail.com>
2021-12-27 10:25:58 -0800

    added email validator to requrements.txt

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -1,6 +1,5 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
-# import email_validator
 from wtforms.validators import DataRequired, Email, ValidationError
 from app.models import User
 
