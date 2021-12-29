cb0624dfe66ea24a7a204cd45a8b2472bbb3734b
Sean Pan <seanhpan@gmail.com>
2021-12-29 10:09:58 -0800

    respond button works

---
+++ react-app/src/components/RespondModal/respond.js
@@ -0,0 +1,38 @@
+import { Link } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { cancelRequest, confirmRequest } from '../../store/friend_list'
+
+function Respond({loggedUser, profile_owner}) {
+    const dispatch = useDispatch()
+
+    const acceptRequest = (adderId) => {
+        dispatch(confirmRequest(adderId, loggedUser.id))
+    }
+
+    const deleteRequest = (adderId) => {
+        dispatch(cancelRequest(adderId, loggedUser.id))
+    }
+
+    return (
+        <>
+            <div key={profile_owner.id} className='individual-friend-request'>
+                <Link className='link-to-friend' to={`/users/${profile_owner.id}`}>
+                    <img className='post-image-wall dim' src={profile_owner?.profile_pic}></img>
+                </Link>
+                <div className='friend-request-minus-portrait'>
+                    <div>
+                        <Link className='link-to-friend' to={`/users/${profile_owner.id}`}>
+                            <span className='requester-name'>{profile_owner?.alias ? profile_owner?.alias : profile_owner?.first_name+' '+profile_owner?.last_name }</span> <span className='sent-you-a-friend-request'>sent you a friend request.</span>
+                        </Link>
+                    </div>
+                    <div className='friend-request-buttons'>
+                        <div className='confirm-friend-request-btn pointer' onClick={() => acceptRequest(profile_owner.id)}>Confirm</div>
+                        <div className='delete-friend-request-btn pointer' onClick={() => deleteRequest(profile_owner.id)}>Delete</div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Respond
