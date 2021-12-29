89cabef07b0bc0d59d845330fe253370eb8959d2
Sean Pan <seanhpan@gmail.com>
2021-12-29 11:39:24 -0800

    footer complete

--- react-app/src/components/SplashPage/splash.js
+++ react-app/src/components/SplashPage/splash.js
@@ -18,7 +18,32 @@ function Splash() {
                 </div>
             </div>
             <footer id='footer'>
-                footer
+                <div className='empty-footer'>
+
+                </div>
+                <div className='footer-container'>
+                    <span className='languages footer-text'>English (US)</span><span className='languages footer-text'>Español</span>
+                    <span className='languages footer-text'>Français (France)</span><span className='languages footer-text'>中文(简体)</span>
+                    <span className='languages footer-text'>العربية</span><span className='languages footer-text'>Português (Brasil)</span>
+                    <span className='languages footer-text'>Italiano</span><span className='languages footer-text'>한국어</span>
+                    <span className='languages footer-text'>Deutsch</span><span className='languages footer-text'>हिन्दी</span>
+                    <span className='languages footer-text'>日本語</span>
+                    <hr style={{marginTop: .7+'rem', marginBottom: .7+'rem'}} size='1' width='100%' color='#dddfe2'></hr>
+                    <div className='footer-last-line'>
+                        <div className="footer-text">
+                           Soundbook <span className='asdf'><span className='at-text'>©</span></span> 2022
+                        </div>
+                        <div className="footer-text">
+                            Developer: Sean Pan
+                        </div>
+                        {/* <div className="footer-text1">
+                            <i class="fab fa-github-square"></i><i class="fab fa-linkedin"></i>
+                        </div> */}
+                    </div>
+                </div>
+                <div className='empty-footer'>
+
+                </div>
             </footer>
         </>
     )
