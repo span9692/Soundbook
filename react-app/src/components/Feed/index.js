fb7de499be11affb203773e5946c562303e420d4
Sean Pan <seanhpan@gmail.com>
2022-01-03 03:07:21 -0800

    edit buttons hop over from each comment or post

--- react-app/src/components/Feed/index.js
+++ react-app/src/components/Feed/index.js
@@ -97,6 +97,7 @@ function Feed({searchParams, setSearchParams}) {
             profile_id: loggedUser.id
         }))
         setPostValue('')
+        closeEmojis()
     }
 
     const removePost = (postId) => {
@@ -330,7 +331,7 @@ function Feed({searchParams, setSearchParams}) {
                                     </div>
                                     <div className='edit-delete-button-container'>
                                         {loggedUser.id === post.owner_id ?
-                                        <div onClick={ () => {editId ? setEditId("") : setEditId(post?.id); setEditValue(post?.post_content); setShowEmojiEditPost(false)} } className='trash-can-post'>
+                                        <div onClick={ () => {editId && editId === post.id ? setEditId("") : setEditId(post?.id); setEditValue(post?.post_content); setShowEmojiEditPost(false)} } className='trash-can-post'>
                                             <i class="fas fa-pencil-alt"></i>
                                         </div>
                                         : null
@@ -393,7 +394,7 @@ function Feed({searchParams, setSearchParams}) {
                                 </div>
                             }
                                 <div class='pointer'>
-                                    <span onClick={() => {commentBoxId ? setCommentBoxId('') : setCommentBoxId(post.id); setShowEmojiComment(false)}} className={commentBoxId === post.id ? 'comment1-button' : 'comment-button'}><i class="far fa-comment"></i> Comment</span>
+                                    <span onClick={() => {commentBoxId && commentBoxId === post.id ? setCommentBoxId('') : setCommentBoxId(post.id); setShowEmojiComment(false)}} className={commentBoxId === post.id ? 'comment1-button' : 'comment-button'}><i class="far fa-comment"></i> Comment</span>
                                 </div>
                             </div>
                             {commentCheck.includes(post?.id) ?
@@ -415,7 +416,7 @@ function Feed({searchParams, setSearchParams}) {
                                                 </Link>
                                                 <div className='comment-icon-position'>
                                                     {loggedUser.id === comment.user_id ?
-                                                    <div className='comment-icon-position' onClick={() => {commentId ? setCommentId('') : setCommentId(comment.id); setEditCommentValue(comment.comment_content); setShowEmojiEditComment(false)}} >
+                                                    <div className='comment-icon-position' onClick={() => {commentId && commentId === comment.id ? setCommentId('') : setCommentId(comment.id); setEditCommentValue(comment.comment_content); setShowEmojiEditComment(false)}} >
                                                         <i class="fas fa-pencil-alt pencil-icon-comment pointer"></i>
                                                     </div>: null
                                                     }
